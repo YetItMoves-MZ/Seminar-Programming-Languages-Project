3d24a35e44368eed2b53bf86729506e3f0bdb7a3
DESKTOP-6O7R7IJ\Ben <yonilol22@gmail.com>
2021-11-15 12:36:01 +0200

    adding listbox logic and display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import ttk
 
+import treeFunctions
 import tkinter as tk
 
 import sqlite3
@@ -7,26 +8,34 @@ import sqlite3
 import createCsvFromDb
 
 
-def view():
-
+def click_event(event):
+    treeFunctions.clear_tree(tree)
     con1 = sqlite3.connect('chinook.db')
-
     cur1 = con1.cursor()
-
-    cur1.execute("SELECT * FROM employees")
-
+    cur1.execute(f"SELECT * FROM {listbox.get(listbox.curselection())}")
     rows = cur1.fetchall()
 
     for row in rows:
-
         tree.insert("", tk.END, values=row)
 
     con1.close()
 
 
+def populate_box(mylistbox):
+    tables_list = ['albums', 'artists', 'customers', 'employees',
+                   'genres', 'invoice_items','invoices', 'media_types',
+                   'playlist_track', 'playlists', 'sqlite_sequence',
+                   'sqlite_stat1', 'tracks']
+    for i in tables_list:
+        mylistbox.insert("end", i)
+
+
+def reset():
+    treeFunctions.clear_tree(tree)
+
+
 # connect to the database
 createCsvFromDb.main()
-
 root = tk.Tk()
 
 tree = ttk.Treeview(root, column=("c1", "c2", "c3"), show='headings')
@@ -45,8 +54,12 @@ tree.heading("#3", text="LNAME")
 
 tree.pack()
 
-button1 = tk.Button(text="Display data", command=view)
-
+button1 = tk.Button(text="Clear Data", command=reset)
 button1.pack(pady=10)
+
+listbox = tk.Listbox(root)
+listbox.pack(pady=20)
+populate_box(listbox)
+listbox.bind('<<ListboxSelect>>', click_event)
 
 root.mainloop()
